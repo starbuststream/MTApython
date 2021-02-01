a4426fc52715485d835ffa8c71aef3ff00d5ec42
starbuststream <78331105+starbuststream@users.noreply.github.com>
2021-02-01 15:25:06 +0800

    Day1.2test.py
    tthth

---
+++ Day1.2test.py
@@ -0,0 +1,15 @@
+score = int(input("分數"))
+if score >=101:
+    print("這成績不對ㄟ")
+elif score>= 90:
+    print("A")
+elif score>= 80:
+    print("B")
+elif score>= 70:
+    print("C")
+elif score>= 60:
+    print("D")
+elif score>=0:
+    print("F")
+else:
+    print("這成績不對A")
