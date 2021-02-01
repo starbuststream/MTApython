cab39f153644ee865f8c4ebebb33dfb584470ae4
starbuststream <78331105+starbuststream@users.noreply.github.com>
2021-02-01 10:45:25 +0800

    Add files via upload

---
+++ Day1.1test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 09:29:09 2021
+
+@author: MTAEXAM
+"""
+height=float(input("高:"))
+top=float(input("上底:"))     
+down=float(input("下底:"))      
+area=(top+down)*height/2
+
+print("梯形面積:"+str(area))
+
